56f0b1234dd86b8e9ff7671300ee440dd65a0b64
Meeribeeri <starstrike3406@gmail.com>
2024-09-04 21:56:37 -0600

    modified:   main.c 	new file:   main.exe 	renamed:    Include/stack.c -> stack.c 	renamed:    Include/stack.h -> stack.h

--- main.c
+++ main.c
@@ -1,18 +1,35 @@
-#include "Include/stack.h"
+//#include "stack.h"
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
 
-extern void canary_setup(void);
-extern unsigned long canary_value;
+//extern void canary_setup(void);
+//extern unsigned long canary_value;
+void canary_tripped(void) {
+    printf("AAAJ");
+}
+
+
+unsigned long canary_value;
+void canary_setup(void)
+{
+     canary_value = 0xBAAAAAAD;//provide some magic numbers
+}
 
 int main() {
     canary_setup();
 
     unsigned long canary;
+
+    int read_bytes;
+
     memcpy(&canary, &canary_value, sizeof(canary_value));
-    //puts(canary_value);
-    //puts(canary);
     char buf[8];
+    printf("canary memory: %p, %p\tbuffer memory: %p\n",canary,canary_value,buf);
+    printf("canary address: %p, %p\tbuffer address: %p\n", (void*)&canary,(void*)&canary_value,(void*)&buf);
     //gets(buf);
-    //puts(canary_value);
-    //puts(canary);
+    scanf("%d", &read_bytes);
+    read(0,buf,read_bytes);
+printf("canary memory: %p, %p\tbuffer memory: %p\n",canary,canary_value,buf);
     printf("%s",buf);
 }
