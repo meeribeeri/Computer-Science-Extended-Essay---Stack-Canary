f97f42f13856a18155ed060b4543c9219c266ed8
Meeribeeri <starstrike3406@gmail.com>
2024-09-05 22:30:56 -0600

    new file:   build/Debug/main.o 	new file:   build/Debug/outDebug.exe 	modified:   main.c 	modified:   main.exe implemented the functions for adding stuff to the stack and the function to be exploited Also implemented 99% of the canary

--- main.c
+++ main.c
@@ -3,17 +3,72 @@
 #include <string.h>
 #include <unistd.h>
 
+#define BUFFER_SIZE 8
+#define RECURSION_MAX 98
 //extern void canary_setup(void);
 //extern unsigned long canary_value;
+
+int selection;
+unsigned short stack_funcs = 0;
+unsigned long canary_value;
+
+//Canary Functions
 void canary_tripped(void) {
     printf("AAAJ");
 }
 
-
-unsigned long canary_value;
 void canary_setup(void)
 {
      canary_value = 0xBAAAAAAD;//provide some magic numbers
+}
+
+void print_memory_status(unsigned long local_canary, char* buffer) {
+    printf("canary value: %p\tglobal canary value: %p\tbuffer memory: %p\n",local_canary,canary_value,buffer);
+    printf("canary address: %p\tglobal canary address %p\tbuffer address: %p\nbuffer value: ", (void*)&local_canary,(void*)&canary_value,(void*)&buffer);
+}
+
+//Functions for the actual tests
+void program_test() {
+    //This is a prime cruncher to test speed & mem
+}
+
+void canary_test() {
+    //This is roppable
+    unsigned long canary;
+    memcpy(&canary, &canary_value, sizeof(canary_value));
+    char buf[BUFFER_SIZE];
+    unsigned int read_bytes;
+
+    print_memory_status(canary, buf);
+
+    printf("Enter the number of maximum bytes to be read: ");
+    scanf("%d", &read_bytes);
+    printf("Enter what to write to the buffer: ");
+    read(0,buf,read_bytes);
+
+    print_memory_status(canary, buf);
+    printf("%s",buf);
+
+    if (memcmp(&canary,&canary_value,sizeof(canary_value))) {
+        canary_tripped();
+    }
+}
+
+void recursive_function_to_have_many_locals_on_the_stack() {
+    unsigned long canary;
+    memcpy(&canary, &canary_value, sizeof(canary_value));
+    if (stack_funcs < RECURSION_MAX) {
+        recursive_function_to_have_many_locals_on_the_stack();
+    } else if (selection == 1) {
+        program_test();
+    } else if (selection == 2) {
+        canary_test();
+    }
+
+
+    if (memcmp(&canary,&canary_value,sizeof(canary_value))) {
+        canary_tripped();
+    }
 }
 
 int main() {
@@ -21,15 +76,19 @@ int main() {
 
     unsigned long canary;
 
-    int read_bytes;
+
+
+    
+    printf("Enter '1' to perform a program test.\nEnter '2' to perform a canary test\nInput: ");
+    scanf("%d", &selection);
 
     memcpy(&canary, &canary_value, sizeof(canary_value));
-    char buf[8];
-    printf("canary memory: %p, %p\tbuffer memory: %p\n",canary,canary_value,buf);
-    printf("canary address: %p, %p\tbuffer address: %p\n", (void*)&canary,(void*)&canary_value,(void*)&buf);
+    
+
     //gets(buf);
-    scanf("%d", &read_bytes);
-    read(0,buf,read_bytes);
-printf("canary memory: %p, %p\tbuffer memory: %p\n",canary,canary_value,buf);
-    printf("%s",buf);
+
+
+    if (memcmp(&canary,&canary_value,sizeof(canary_value))) {
+        canary_tripped();
+    }
 }
