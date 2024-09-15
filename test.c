7fea5c037fec03e369fae4aae4d826fc28a817f4
meeribeeri <starstrike3406@gmail.com>
2024-09-14 18:39:15 -0600

    modified:   1ByteCanaryData.txt 	modified:   Canary.txt 	modified:   exploit.py 	new file:   test 	new file:   test.c
    canary tes python script done

---
+++ test.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <time.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+long double a = 0;
+uint64_t b = 0;
+
+int main() {
+
+
+    printf("\n%i\n",sizeof(a));
+    printf("%i\n",sizeof(b));
+}
